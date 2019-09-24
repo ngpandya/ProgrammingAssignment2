6530c690d66b2cba0db960e69512c99c3db9fe6a
ngpandya <50445006+ngpandya@users.noreply.github.com>
2019-09-24 18:59:06 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,29 @@
 ## functions do
 
 ## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  inver <- x
+  set <- function(y) {
+    x <<- y
+    inver <<- x
+  }
+  get <- function() x
+  setinver <- function(invererse) inver <<- invererse
+  final <- function(inver) 
+    list(set = set, get = get, setinver = setinver, final = final)
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  inver <- x$final()
+  if(!is.null(inver)) {
+    message("cached data")
+    return(inver)
+  }
+  data <- x$get()
+  inver <- solve(data, ...)
+  x$setinver(inver)
+  inver
 }
